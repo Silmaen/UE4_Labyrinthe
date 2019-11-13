1dae3b9a60a539ad057e2cf47d5801a2b9718102
Damien Lachouette <lachouette.damien@gmail.com>
2019-11-13 21:58:39 +0100

    creation of UE Project

---
+++ Source/UE4_Labyrinthe/UE4_Labyrinthe.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "UE4_Labyrinthe.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, UE4_Labyrinthe, "UE4_Labyrinthe" );
---
+++ Source/UE4_Labyrinthe/UE4_LabyrintheGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "UE4_LabyrintheGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UE4_LABYRINTHE_API AUE4_LabyrintheGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
